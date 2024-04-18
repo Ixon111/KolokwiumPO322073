81828e72b59e71ab8fbbf65cc5eab663e6b7e35c
Ixon <igorjaroniewski@gmail.com>
2024-04-18 12:20:25 +0200

    Initial Commit

---
+++ KolokwiumPO322073/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KolokwiumPO322073
+//
+//  Created by Igor Jaroniewski on 18/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
